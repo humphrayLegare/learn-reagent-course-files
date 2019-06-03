d04bb1fa9e8171947623163a2d7129ea47b82660
hlegare <humphray.legare@libeo.com>
2019-06-02 22:04:54 -0400

    added the main in gigs.cljs

--- giggin/src/giggin/components/gigs.cljs
+++ giggin/src/giggin/components/gigs.cljs
@@ -1,4 +1,13 @@
-(ns giggin.components.gigs)
+(ns giggin.components.gigs
+  (:require [giggin.state :as state]))
 
 (def gigs(fn []
-  [:gigs "gigs"]))
+  [:main
+  [:div.gigs (map (fn [gig] [:div.gig {:key (get gig :id)}
+                              [:img.gig__artwork {:src (get gig :img) :alt (get gig :title)}]
+                              [:div.gig__body
+                              [:div.gig__title
+                              [:div.btn.btn--primary.float--right.tooltip {:data-tooltip "Add to Order"}
+                              [:i.icon.icon--plus]] (get gig :title)]
+                              [:p.gig__price (get gig :price)]
+                              [:p.gig__desc (get gig :desc)]]]) (vals @state/gigs))]]))
